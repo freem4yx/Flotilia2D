f4781ce54d3d076a3a75cb68df23e4ef086e88b7
freem4yx <freem4yx@yandex.ru>
2024-10-11 15:50:07 +0300

    freem-ver upd.

--- Assets/another/Scripts/EnemyShip.cs
+++ Assets/another/Scripts/EnemyShip.cs
@@ -8,18 +8,27 @@ public class EnemyShip : MonoBehaviour
     public static int hp = 100;
     public int damage = 10;
 
+    public Sprite damaged1;
+    public Sprite damaged2;
+
     private Animator anim;
     public MyShip me;
+
+    public HealthBar healthBarEnemy;
+
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
         anim = GetComponent<Animator>();
         hp = maxHP;
+        healthBarEnemy.SetMaxHealth(hp);
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        if (MyShip.canAttack == false) 
+        if (MyShip.canAttack == false && hp > 0) 
         {
             Debug.Log("Enemy " + hp);
             anim.SetTrigger("ShootOn");
@@ -35,9 +44,12 @@ public class EnemyShip : MonoBehaviour
         me.SetHealth(me.getHealth() - damage);
     }
 
-    public void SetHealth(int healthp)
+    public void SetHealth(int health)
     {
-        hp = healthp;
+        hp = health;
+        healthBarEnemy.SetHealth(hp);
+        if (hp <= 50) spriteRenderer.sprite = damaged1;
+        if (hp <= 0) spriteRenderer.sprite = damaged2;
     }
 
     public int getHealth()
--- Assets/another/Scripts/MyShip.cs
+++ Assets/another/Scripts/MyShip.cs
@@ -6,16 +6,25 @@ public class MyShip : MonoBehaviour
 {
     public int maxHP = 100;
     public static int hp = 100;
-    public int damage = 10;
+    public int damage = 20;
+
+    public Sprite damaged1;
+    public Sprite damaged2;
 
     private Animator anim;
     public EnemyShip enemy;
+
+    public HealthBar healthBar;
+
+    private SpriteRenderer spriteRenderer;
 
     public static bool canAttack = true;
     void Start()
     {
         anim = GetComponent<Animator>();
         hp = maxHP;
+        healthBar.SetMaxHealth(hp);
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -32,11 +41,14 @@ public class MyShip : MonoBehaviour
     public void SetHealth(int healthp)
     {
         hp = healthp;
+        healthBar.SetHealth(hp);
+        if (hp <= 50) spriteRenderer.sprite = damaged1;
+        if (hp <= 0) spriteRenderer.sprite = damaged2;
     }
 
     void Attack(EnemyShip enemy)
     {
-        enemy.SetHealth(enemy.getHealth() - damage); 
+        enemy.SetHealth(enemy.getHealth() - damage);
     }
 
     public int getHealth()
